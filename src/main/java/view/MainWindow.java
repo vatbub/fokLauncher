119e6ad1caab9d96e9d61a6812af47c80e5b959c
Frederik Kammel <frederikkammel@outlook.de>
2016-07-31 02:25:47 +0200

    Removed maven sample files and added beginnings of MainWindow

---
+++ src/main/java/view/MainWindow.java
@@ -0,0 +1,18 @@
+package view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainWindow extends Application {
+	
+	public void main (String[] args){
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage arg0) throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+}
