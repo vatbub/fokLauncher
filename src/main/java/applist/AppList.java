36cefaad9d7268c6a3509f8a8250186ca90cf4e6
Frederik Kammel <frederikkammel@outlook.de>
2016-08-18 20:03:00 +0200

    Added getAppByMavenCoordinates

--- src/main/java/applist/AppList.java
+++ src/main/java/applist/AppList.java
@@ -30,22 +30,68 @@ public class AppList extends ArrayList<App> {
 	 * Clears the version cache of all apps in this list
 	 */
 	public void clearVersionCache() {
-		for(App app:this){
+		for (App app : this) {
 			app.latestOnlineSnapshotVersion = null;
 			app.latestOnlineVersion = null;
 			app.onlineVersionList = null;
 		}
-		
+
 		reloadContextMenuEntriesOnShow();
 	}
-	
+
 	/**
 	 * Tells the context menu off all apps to reload its version lists
 	 */
-	public void reloadContextMenuEntriesOnShow(){
-		for(App app:this){
+	public void reloadContextMenuEntriesOnShow() {
+		for (App app : this) {
 			app.setDeletableVersionListLoaded(false);
 			app.setSpecificVersionListLoaded(false);
 		}
+	}
+
+	/**
+	 * Searches this {@link AppList} for the specified app.
+	 * 
+	 * @param mavenGroupId
+	 *            The maven groupId of the app to find
+	 * @param mavenArtifactId
+	 *            The maven artifactId of the app to find
+	 * @return The first matching {@link app} or {@code null} if no app matches.
+	 */
+	public App getAppByMavenCoordinates(String mavenGroupId, String mavenArtifactId) {
+		return getAppByMavenCoordinates(mavenGroupId, mavenArtifactId, null);
+	}
+
+	/**
+	 * Searches this {@link AppList} for the specified app.
+	 * 
+	 * @param mavenGroupId
+	 *            The maven groupId of the app to find
+	 * @param mavenArtifactId
+	 *            The maven artifactId of the app to find
+	 * @param mavenClassifier
+	 *            The maven artifactId of the app to find
+	 * @return The first matching {@link app} or {@code null} if no app matches.
+	 */
+	public App getAppByMavenCoordinates(String mavenGroupId, String mavenArtifactId, String mavenClassifier) {
+		for (App app : this) {
+			if (app.getMavenGroupID().equals(mavenGroupId) && app.getMavenArtifactID().equals(mavenArtifactId)) {
+				// ArtifactId and groupId match, check if classifier matches too
+				// if required
+				if (mavenClassifier != null) {
+					// We need to check the classifier too
+					if (app.getMavenClassifier().equals(mavenClassifier)) {
+						// Everything matches
+						return app;
+					}
+				} else {
+					// Only artifactId and groupId are required to match
+					return app;
+				}
+			}
+		}
+
+		// We only arrived here if no match was found
+		return null;
 	}
 }
