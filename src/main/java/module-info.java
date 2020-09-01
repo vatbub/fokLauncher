73e06f879a049da62ecff222c72f14a841459063
Frederik Kammel <vatbub123+github@googlemail.com>
2020-09-01 14:07:46 +0200

    First attepmts to upgrade to JDK 14

---
+++ src/main/java/module-info.java
@@ -0,0 +1,21 @@
+module foklauncher {
+    requires common.core;
+    requires common.updater;
+    requires javafx.graphics;
+    requires javafx.controls;
+    requires mslinks;
+    requires org.apache.commons.io;
+    requires commons.lang;
+    requires jdom2;
+    requires org.jetbrains.annotations;
+    requires java.desktop;
+    requires java.logging;
+    requires awaitility;
+    requires javafx.fxml;
+    requires commons.cli;
+    requires common.internet;
+    requires common.view.core;
+    requires common.view.motd;
+    requires com.rometools.rome;
+    requires metrics.datadog;
+}
